a9447bfc4fe31bed23e57bebed288ee636da2b7c
Rachel <me@rcabot.com>
2015-10-16 20:24:17 +0100

    Distance functionality implemented.

--- PaddleBeat/Assets/NodeSelector.cs
+++ PaddleBeat/Assets/NodeSelector.cs
@@ -24,31 +24,14 @@ public class NodeSelector : MonoBehaviour {
 	public List<GameObject> nodes;
 	public List<Tuple<MoveType, int>> sequence;
 	public int currentStep;
+    public GameObject currentNode;
 	public BallMovement movement;
 	// Use this for initialization
 	void Start () {
 
 		sequence = new List<Tuple<MoveType, int>>(){
-			new Tuple<MoveType, int>(MoveType.DEFAULT,1),
 			new Tuple<MoveType, int>(MoveType.DEFAULT,0),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,1),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,2),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,3),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,5),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,10),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,9),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,8),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,7),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,6),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,5),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,4),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,3),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,1),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,3),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,0),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,2),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,10),
-			new Tuple<MoveType, int>(MoveType.DEFAULT,0)
+			new Tuple<MoveType, int>(MoveType.DEFAULT,1)
 		};
 
 		movement.nodeNext=nodes[sequence[currentStep].Second];
@@ -57,17 +40,21 @@ public class NodeSelector : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		timer=timer+Time.deltaTime;
-		if(timer>CURRENT_SPEED)
+        if (timer > CURRENT_SPEED / 2)
+        {
+            currentNode = nodes[sequence[currentStep].Second];
+        }
+		if(timer > CURRENT_SPEED)
 		{
-
-			Debug.Log (sequence.Count);
-			Vector3 lastNodePosition = nodes[sequence[currentStep].Second].transform.position;
+			Vector3 lastNodePosition = 
+                nodes[sequence[currentStep].Second].transform.position;
 			timer=0;
 			currentStep++;
 			if(currentStep==sequence.Count)
 				currentStep=0;
 			movement.nodeNext=nodes[sequence[currentStep].Second];
-			movement.speed=Vector3.Distance(lastNodePosition, nodes[sequence[currentStep].Second].transform.position)/CURRENT_SPEED;
+			movement.speed=Vector3.Distance(lastNodePosition, 
+                nodes[sequence[currentStep].Second].transform.position)/CURRENT_SPEED;
 		}
 	}
 }
