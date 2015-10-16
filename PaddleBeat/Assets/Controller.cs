74e63e6f03be61f87073a34fa5ce1ce08b84f2dd
Rachel <me@rcabot.com>
2015-10-16 22:54:59 +0100

    Forgot to commit extra files.

---
+++ PaddleBeat/Assets/Controller.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+    public BallMovement ball;
+    public GraceManager grace;
+    public NodeSelector nodeSystem;
+    public float distance;
+    public float maxDistance;
+    public bool playerHasHitThisStep=false;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyUp(KeyCode.Space)&&!grace.isGraced())
+        {
+            distance = Vector3.Distance(ball.gameObject.transform.position, 
+                nodeSystem.currentNode.transform.position);
+            Debug.Log(distance);
+            playerHasHitThisStep = true;
+            if(distance>maxDistance)
+            {
+                grace.grace();
+            }
+        }
+	}
+
+    public void checkHasPressed()
+    {
+        Debug.Log("checking...");
+        if(playerHasHitThisStep)
+        {
+            playerHasHitThisStep = false;
+        }
+        else if(!grace.isGraced())
+        {
+            grace.grace();
+        }
+
+    }
+}
---
+++ PaddleBeat/Assets/GraceManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraceManager : MonoBehaviour
+{
+
+    private bool graced = false;
+    public float periodLength;
+
+    public void grace()
+    {
+        if(!graced) StartCoroutine(gracePeriod());
+    }
+
+    public bool isGraced()
+    {
+        return graced;
+    }
+
+    IEnumerator gracePeriod()
+    {
+        graced = true;
+        startGrace();
+        yield return new WaitForSeconds(periodLength);
+        endGrace();
+        graced = false;
+    }
+
+    private void startGrace()
+    {
+        Debug.Log("grace started.");
+    }
+
+    private void endGrace()
+    {
+        Debug.Log("grace end.");
+    }
+
+}
