ee3440376593e8d86f180307c39875b938343d0a
Rachel <me@rcabot.com>
2015-10-16 22:51:47 +0100

    Grace period in place - will probably need to use hit regions around the nodes in stead of halfway checking.

--- PaddleBeat/Assets/NodeSelector.cs
+++ PaddleBeat/Assets/NodeSelector.cs
@@ -23,38 +23,43 @@ public class NodeSelector : MonoBehaviour {
 	public float timer;
 	public List<GameObject> nodes;
 	public List<Tuple<MoveType, int>> sequence;
-	public int currentStep;
+	private int currentNodeStep;
     public GameObject currentNode;
-	public BallMovement movement;
-	// Use this for initialization
-	void Start () {
+    public int currentStep;
+    public BallMovement movement;
+    public Controller controller;
 
+	// Use this for initialization
+	void Start ()
+    {
 		sequence = new List<Tuple<MoveType, int>>(){
 			new Tuple<MoveType, int>(MoveType.DEFAULT,0),
 			new Tuple<MoveType, int>(MoveType.DEFAULT,1)
 		};
 
-		movement.nodeNext=nodes[sequence[currentStep].Second];
+		movement.nodeNext=nodes[sequence[currentNodeStep].Second];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timer=timer+Time.deltaTime;
-        if (timer > CURRENT_SPEED / 2)
+        if (timer > CURRENT_SPEED / 2 && currentNode!= movement.nodeNext)
         {
-            currentNode = nodes[sequence[currentStep].Second];
+            currentStep = sequence[currentNodeStep].Second;
+            currentNode = nodes[currentStep];
+            controller.checkHasPressed();
         }
 		if(timer > CURRENT_SPEED)
 		{
 			Vector3 lastNodePosition = 
-                nodes[sequence[currentStep].Second].transform.position;
+                nodes[sequence[currentNodeStep].Second].transform.position;
 			timer=0;
-			currentStep++;
-			if(currentStep==sequence.Count)
-				currentStep=0;
-			movement.nodeNext=nodes[sequence[currentStep].Second];
+			currentNodeStep++;
+			if(currentNodeStep==sequence.Count)
+				currentNodeStep=0;
+			movement.nodeNext=nodes[sequence[currentNodeStep].Second];
 			movement.speed=Vector3.Distance(lastNodePosition, 
-                nodes[sequence[currentStep].Second].transform.position)/CURRENT_SPEED;
+                nodes[sequence[currentNodeStep].Second].transform.position)/CURRENT_SPEED;
 		}
 	}
 }
